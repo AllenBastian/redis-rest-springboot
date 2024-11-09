2bf7496b0d2b95e88945b168d58f743fe389f4bc
AllenBastian <reachallenbastian@gmail.com>
2024-11-09 22:45:44 +0530

    ADDED: add entity for todo

--- src/main/java/com/todo/demo/controller/CrudController.java
+++ src/main/java/com/todo/demo/controller/CrudController.java
@@ -1,10 +1,27 @@
 package com.todo.demo.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/todo")
 public class CrudController {
+
+    @GetMapping("/get-all")
+    public String getAllTodos(){
+        return "hello checking";
+    }
+
+//    @PostMapping("/add")
+//    public String addTodo(){
+//        return "added";
+//    }
+//
+//
+//    @DeleteMapping("/delete/{id}")
+//    public String deleteTodo(){
+//        return "deleted";
+//    }
 
 
 }
---
+++ src/main/java/com/todo/demo/entities/TodoList.java
@@ -0,0 +1,41 @@
+package com.todo.demo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class TodoList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String todoList;
+
+
+    public TodoList(){}
+
+    public TodoList(Long id,String todoList){
+        this.id = id;
+        this.todoList = todoList;
+    }
+
+
+    public void setTodoList(String todoList) {
+        this.todoList = todoList;
+    }
+
+    public String getTodoList() {
+        return todoList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
