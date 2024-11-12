04c77564a1abe276dccbc6872ae3ba1a7f060abf
AllenBastian <reachallenbastian@gmail.com>
2024-11-12 11:14:41 +0530

    ADDED: security config

--- src/main/java/com/todo/demo/services/UserService.java
+++ src/main/java/com/todo/demo/services/UserService.java
@@ -5,7 +5,13 @@ import com.github.f4b6a3.ulid.UlidCreator;
 import com.todo.demo.entities.User;
 import com.todo.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class UserService {
@@ -21,6 +27,10 @@ public class UserService {
     public User createUser(User user){
 
         Ulid ulid = UlidCreator.getUlid();
+        Map encoders = new HashMap<>();
+        encoders.put("bcrypt",new BCryptPasswordEncoder());
+        PasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("bcrypt",encoders);
+        System.out.println(passwordEncoder.encode("allen"));
         user.setPassword("daiveekam");
         user.setId(ulid.toString());
         return userRepository.save(user);
