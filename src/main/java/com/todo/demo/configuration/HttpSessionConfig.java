251b175346818de1a57af99e47d299131215987c
AllenBastian <reachallenbastian@gmail.com>
2024-11-22 22:02:54 +0530

    ADDED: cookie customizing bean CookieSerializer

--- src/main/java/com/todo/demo/configuration/HttpSessionConfig.java
+++ src/main/java/com/todo/demo/configuration/HttpSessionConfig.java
@@ -7,10 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 
 import java.io.Serializable;
-
 
 @Configuration
 @EnableRedisHttpSession
@@ -22,7 +23,13 @@ public class HttpSessionConfig {
         return new LettuceConnectionFactory();
     }
 
-
-
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("todoSession");
+        serializer.setCookiePath("/");
+        serializer.setDomainNamePattern("^.+?\\.(\\w+\\.[a-z]+)$");
+        return serializer;
+    }
 
 }
