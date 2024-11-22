c37f46ccc148f1b0603515cc1b4b798de36992e0
AllenBastian <reachallenbastian@gmail.com>
2024-11-22 20:14:41 +0530

    ADDED: jdbc

--- src/main/java/com/todo/demo/configuration/HttpSessionConfig.java
+++ src/main/java/com/todo/demo/configuration/HttpSessionConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 
 import java.io.Serializable;
@@ -20,6 +22,15 @@ public class HttpSessionConfig {
     @Bean
     public LettuceConnectionFactory connectionFactory() {
         return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("JSESSIONID");
+        serializer.setCookiePath("/");
+        serializer.setDomainNamePattern("^.+?\\.(\\w+\\.[a-z]+)$");
+        return serializer;
     }
 
 
